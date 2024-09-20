d8819793d4103f09389e4b6b1889d39cbefdd576
Yunus <152853532+YunusSotiboldiyev@users.noreply.github.com>
2024-09-20 19:00:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function applyStyle() {
+     let param = document.querySelector(".param1");
+     param.style.fontFamily = "Calibri";
+     let red = document.querySelector(".red");
+     red.style.color = "red";
+     let bigger = document.querySelector(".bigger");
+     bigger.style.fontSize = "25px";
+     let green = document.querySelector(".green");
+     green.style.color = "green";
+     let middle = document.querySelector(".middle");
+     middle.style.width = "max-content";
+     middle.style.margin = "auto";
+     middle.style.color = "rgba(0,0,0,0.6)";
+     middle.style.fontSize = "20px";
+     let redLi = document.querySelector(".red2");
+     redLi.style.color = "red";
+     let blueLi = document.querySelector(".blue");
+     blueLi.style.color = "blue";
+     blueLi.style.fontSize = "20px";
+     let fixedGreen = document.querySelector(".fixedGreen");
+     fixedGreen.style.color = "green";
+     fixedGreen.style.fontSize = "22px";
+     let btn = document.querySelector(".btn");
+     btn.style.padding = "10px 5px";
+     btn.style.backgroundColor = "green";
+     btn.style.border = "none";
+ }
+ 
+         
